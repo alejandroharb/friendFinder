41332b2e52de0ece5c2ad96f6a5b7b2b4b2a82cc
alejandro harb <alejandro harb>
2017-02-07 22:20:53 -0600

    added survey form

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -1,12 +1,6 @@
+
 $(document).ready(function() {
-    var queryURL = '/api/questions'
-    $.ajax({url: queryURL, method: 'GET'}).done(function(response){
-        for(var i = 0; i < response.length; i++) {
-            var n = i + 1;
-            var q = response[i];
-            console.log(q)
-            var newLi = $('<li>').text(n + '. ' + q);
-            $('#surveyQuestions').append(newLi);
-        }
-    });
+    $('select').material_select();
+    $('#textarea1').val('New Text');
+    $('#textarea1').trigger('autoresize');
 })
